2c6c3d1abdc027c08fca6647cfa92e9f9dbafd81
Sally Erisman <sally.erisman@gmail.com>
2020-01-10 11:40:11 +0100

    Changed date input field to only include day and month. Created functions for rendering dates and names.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,45 +1,62 @@
 const searchForm = document.querySelector("#search-form");
-
-
-// Function for getting user search and empytying the search field
-const getNameSearch = function() {   
-    const inputName = document.querySelector('#name');
-    const nameSearch = inputName.value;
-    inputName.value = "";
-    return nameSearch;
-};
-
-// Function for getting user search and empytying the search field
-const getDateSearch = function() {   
-    const inputDate = document.querySelector('#date');
-    const dateSearch = inputDate.value;
-    inputDate.value = "";
-    return dateSearch;
-};
-
+const day = document.querySelector('#day').value;
+const month = document.querySelector('#month').value;
+let inputName = document.querySelector('#name').value;
 
 //Function for rendering date of specific name day
 const renderDay = name => {
 
     name.results.forEach(nameEl => {
-        if (nameEl.name !== "Anna") {
-            return; 
+        if (nameEl.name !== inputName[0].toUpperCase() + inputName.slice(1)) {
+            console.log("Soory, no name day"); 
         } else {
             document.querySelector("#result-wrapper").innerHTML = `
             <div class="col-sm-12 col-md-10 col-lg-8 result">
                 <h1 class="title">${nameEl.name}</h1>
-                <p class="card-text">${nameEl.name} has a name day on ${nameEl.day} ${nameEl.month}</p>
+                <p class="card-text">${nameEl.name} has a name day on ${nameEl.day}/${nameEl.month}</p>
             </div>
         `;
         }
     })
 };
 
+
+//Function for rendering date of specific name day
+// const renderNames = date => {
+//     document.querySelector("#result-wrapper").innerHTML = `
+//     <div class="col-sm-12 col-md-10 col-lg-8 result">
+//         <h1 class="title">${day}/${month}</h1>
+//         <p class="card-text">${date.data[0].namedays.se} has a name day!</p>
+//     </div>
+//     `;   
+// };
+
 // Event listener:
+// searchForm.addEventListener('submit', function(e) {
+//     e.preventDefault();
+
+// 	getDayByName(getSearch())
+// 		.then(name => {
+//             renderDay(name);
+// 		})
+// 		.catch(err => {
+// 			alert("Error:", err);
+// 		});
+
+// });
+
 searchForm.addEventListener('submit', function(e) {
     e.preventDefault();
 
-	getDayByName(getSearch())
+	// getNamesByDay(month, day)
+	// 	.then(date => {
+    //         renderNames(date);
+	// 	})
+	// 	.catch(err => {
+	// 		alert("Error:", err);
+    // 	});
+    
+    	getDayByName(inputName)
 		.then(name => {
             renderDay(name);
 		})
