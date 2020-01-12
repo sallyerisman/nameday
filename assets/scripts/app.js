a7ed583d50583e1bcc071557d765e685f26478ad
Sally Erisman <sally.erisman@gmail.com>
2020-01-12 23:24:27 +0100

    Created new API request, render function, button for viewing today's name days.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,21 +6,21 @@
 const searchForm = document.querySelector("#search-form");
 const resultWrapper = document.querySelector("#result-wrapper");
 
-const noMatch = () => {
+const renderNoMatch = () => {
     resultWrapper.innerHTML = `<div class="alert alert-warning">Tyvärr, vi kunde inte hitta en namnsdag för detta namn.</div>`;
+};
+
+// Function for rendering error message
+const renderError = () => {
+    resultWrapper.innerHTML = `<div class="alert alert-danger">Sökning misslyckades: Det gick inte att hämta data för denna begäran.</div>`;
 };
 
 //Function for rendering date of specific name day
 const renderDay = (name, nameSearch) => {
+    // Capitalize name input
+    nameSearch = nameSearch[0].toUpperCase() + nameSearch.slice(1);
     name.results.forEach(nameEl => {
-        
-        // Capitalize name input
-        nameSearch = nameSearch[0].toUpperCase() + nameSearch.slice(1);
-        
-        if (nameEl.name !== nameSearch) {
-            noMatch();
-        } else {
-        resultWrapper.innerHTML = `
+            resultWrapper.innerHTML = `
             <div class="col-sm-12 col-md-10 col-lg-8 result">
                 <h2 class="title">${nameSearch}</h2>
                 <p class="card-text">har namnsdag den ${nameEl.day}/${nameEl.month}</p>
@@ -28,7 +28,6 @@ const renderDay = (name, nameSearch) => {
                 <img class="cake" src="assets/images/cake.png" alt="Cake"> 
             </div>
             `;
-        }
     });
 };
 
@@ -37,15 +36,22 @@ const renderNames = (date, country, month, day) => {
     resultWrapper.innerHTML = `
     <div class="col-sm-12 col-md-10 col-lg-8 result">
         <h2 class="title">${day}/${month}</h2>
-        <p class="card-text">På denna dag i har ${date.data[0].namedays[country]} namnsdag!</p>
+        <p class="card-text">På denna dag har ${date.data[0].namedays[country]} namnsdag!</p>
         <img class="cake" src="assets/images/cake.png" alt="Cake">  
     </div>
     `;   
 };
 
-// Function for rendering error message
-const renderError = () => {
-    resultWrapper.innerHTML = `<div class="alert alert-danger">Sökning misslyckades: Det gick inte att hämta data för denna begäran.</div>`;
+//Function for rendering who has a name day today
+const renderNameday = (date, country) => {
+
+    resultWrapper.innerHTML = `
+    <div class="col-sm-12 col-md-10 col-lg-8 result">
+        <h2 class="title">GRATTIS</h2>
+        <p class="card-text">${date.data[0].namedays[country]} på namnsdagen!</p>
+        <img class="cake" src="assets/images/cake.png" alt="Cake">  
+    </div>
+    `;   
 };
 
 // Event listener for radio buttons
@@ -54,11 +60,32 @@ document.querySelector(".radio-buttons").addEventListener("click", function(e) {
     if (document.querySelector("#by-name").checked) {
         document.querySelector("#name-search").classList.add("show");
         document.querySelector("#date-search").classList.remove("show");
+        document.querySelector("#timezone-selection").classList.remove("show");
+        document.querySelector("#todays").classList.remove("show");    
 
 	} else if (document.querySelector("#by-date").checked) {
         document.querySelector("#date-search").classList.add("show");
+        document.querySelector("#timezone-selection").classList.add("show");
+        document.querySelector("#todays").classList.add("show");         
         document.querySelector("#name-search").classList.remove("show");
     }
+});
+
+// Event listener for cake button
+document.querySelector(".todays").addEventListener("click", function(e) {
+   
+    const timezone = document.querySelector("#timezone").value;
+    const country = document.querySelector("#country").value;
+    
+    getTodaysNamedays(country, timezone)
+		.then(date => {
+            if(200) {
+                renderNameday(date, country);
+            } else {
+                renderError();
+            } 
+		})
+		.catch(renderError);
 });
 
 // Event listener for submit button
@@ -67,10 +94,24 @@ searchForm.addEventListener("submit", function(e) {
 
     const day = document.querySelector("#day").value;
     const month = document.querySelector("#month").value;
-    const country = document.querySelector("#country").value;
     const nameSearch = document.querySelector("#name").value;
+    const country = document.querySelector("#country").value;
 
-    if (day & month) {
+    if (nameSearch) {
+        getDayByName(nameSearch, country)
+		.then(name => {
+            if(200) {
+                if (name.results.length > 0) {
+                    renderDay(name, nameSearch);
+                } else {
+                    renderNoMatch();
+                }                
+            } else {
+                renderError();
+            }   
+		})
+        .catch(renderError);
+    } else if (day) {
         getNamesByDay(country, month, day)
 		.then(date => {
             if(200) {
@@ -79,21 +120,7 @@ searchForm.addEventListener("submit", function(e) {
                 renderError();
             } 
 		})
-		.catch(renderError);
-    } else if (nameSearch) {
-        getDayByName(nameSearch, country)
-		.then(name => {
-            if(200) {
-                if (name.results.length > 0) {
-                    renderDay(name, nameSearch);
-                } else {
-                    noMatch();
-                }                
-            } else {
-                renderError();
-            }   
-		})
-		.catch(renderError);    
+		.catch(renderError);  
     } else {
         resultWrapper.innerHTML = `<div class="alert alert-warning">Sökning misslyckades. Vänligen sök på antingen namn eller datum.</div>`;
     }
--- assets/scripts/namedayAPI.js
+++ assets/scripts/namedayAPI.js
@@ -6,10 +6,10 @@
 // Fetch API data based on name search
 const getDayByName = async(name, country) => {
     // Get date of a specific name day
-    const response = await fetch(`https://api.abalin.net/getdate?name=${name}&country=${country}`);
+    const dayResp = await fetch(`https://api.abalin.net/getdate?name=${name}&country=${country}`);
 
     // Convert response from JSON
-    const day = await response.json();
+    const day = await dayResp.json();
 
     return day;
 };
@@ -17,10 +17,21 @@ const getDayByName = async(name, country) => {
 // Fetch API data based on date search
 const getNamesByDay = async(country, month, day) => {
     // Get name day on a specific date
-    const response = await fetch(`https://api.abalin.net/namedays?country=${country}&month=${month}&day=${day}`);
+    const nameResp = await fetch(`https://api.abalin.net/namedays?country=${country}&month=${month}&day=${day}`);
 
     // Convert response from JSON
-    const names = await response.json();
+    const names = await nameResp.json();
 
     return names;
+};
+
+// Fetch API data for today's name day, country and time zone
+    const getTodaysNamedays = async(country, timezone) => {
+    // Get name day for today
+    const namedayResp = await fetch(`https://api.abalin.net/today?timezone=${timezone}&country=${country}`);    
+
+    // Convert response from JSON
+    const nameDay = await namedayResp.json();
+
+    return nameDay;
 };
