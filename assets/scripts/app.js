0f9e33773d1e920848bf9a9b8d293e5c76200547
Sally Erisman <sally.erisman@gmail.com>
2020-01-14 07:11:54 +0100

    Improved layout. Changed render function to compensate for inconsistencies in JSON.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -19,12 +19,29 @@ const renderError = () => {
 const renderDay = (name, nameSearch) => {
     // Capitalize name input
     nameSearch = nameSearch[0].toUpperCase() + nameSearch.slice(1);
-    name.results.forEach(nameEl => {
+
+    // Find and render exact match when delivered multiple results
+    name.results.find(nameEl => {
+
+        if (nameEl.name === nameSearch) {
             resultWrapper.innerHTML = `
             <div class="col-sm-12 col-md-10 col-lg-8 result">
                 <h2 class="title">${nameSearch}</h2>
                 <p class="card-text">har namnsdag den ${nameEl.day}/${nameEl.month}</p>
                 <p class="card-text">Här är alla som har namnsdag denna dag: ${nameEl.name}.</p>
+                <img class="cake" src="assets/images/cake.png" alt="Cake"> 
+            </div>
+            `;
+        } 
+    });
+
+    // Render result with multiple names 
+    name.results.forEach(nameElSecond => {
+        resultWrapper.innerHTML = `
+            <div class="col-sm-12 col-md-10 col-lg-8 result">
+                <h2 class="title">${nameSearch}</h2>
+                <p class="card-text">har namnsdag den ${nameElSecond.day}/${nameElSecond.month}</p>
+                <p class="card-text">Här är alla som har namnsdag denna dag: ${nameElSecond.name}.</p>
                 <img class="cake" src="assets/images/cake.png" alt="Cake"> 
             </div>
             `;
@@ -36,7 +53,7 @@ const renderNames = (date, country, month, day) => {
     resultWrapper.innerHTML = `
     <div class="col-sm-12 col-md-10 col-lg-8 result">
         <h2 class="title">${day}/${month}</h2>
-        <p class="card-text">På denna dag har ${date.data[0].namedays[country]} namnsdag!</p>
+        <p class="card-text">Den ${day}/${month} har ${date.data[0].namedays[country]} namnsdag!</p>
         <img class="cake" src="assets/images/cake.png" alt="Cake">  
     </div>
     `;   
@@ -44,11 +61,10 @@ const renderNames = (date, country, month, day) => {
 
 //Function for rendering who has a name day today
 const renderNameday = (date, country) => {
-
     resultWrapper.innerHTML = `
     <div class="col-sm-12 col-md-10 col-lg-8 result">
-        <h2 class="title">GRATTIS</h2>
-        <p class="card-text">${date.data[0].namedays[country]} på namnsdagen!</p>
+        <h2 class="title">${date.data[0].namedays[country]}</h2>
+        <p class="card-text">Grattis på namnsdagen!</p>
         <img class="cake" src="assets/images/cake.png" alt="Cake">  
     </div>
     `;   
@@ -58,21 +74,17 @@ const renderNameday = (date, country) => {
 document.querySelector(".radio-buttons").addEventListener("click", function(e) {
     
     if (document.querySelector("#by-name").checked) {
-        document.querySelector("#name-search").classList.add("show");
-        document.querySelector("#date-search").classList.remove("show");
-        document.querySelector("#timezone-selection").classList.remove("show");
-        document.querySelector("#todays").classList.remove("show");    
+        document.querySelector(".name-search").classList.add("show");
+        document.querySelector(".date-search").classList.remove("show");
 
 	} else if (document.querySelector("#by-date").checked) {
-        document.querySelector("#date-search").classList.add("show");
-        document.querySelector("#timezone-selection").classList.add("show");
-        document.querySelector("#todays").classList.add("show");         
-        document.querySelector("#name-search").classList.remove("show");
+        document.querySelector(".date-search").classList.add("show");      
+        document.querySelector(".name-search").classList.remove("show");
     }
 });
 
 // Event listener for cake button
-document.querySelector(".todays").addEventListener("click", function(e) {
+document.querySelector("#cake-button").addEventListener("click", function(e) {
    
     const timezone = document.querySelector("#timezone").value;
     const country = document.querySelector("#country").value;
@@ -85,7 +97,8 @@ document.querySelector(".todays").addEventListener("click", function(e) {
                 renderError();
             } 
 		})
-		.catch(renderError);
+        .catch(renderError);
+        searchForm.reset();
 });
 
 // Event listener for submit button
